26832e613c47d158ebebde3e9d9eace4e85e7a37
w3WebTech <164163622+w3WebTech@users.noreply.github.com>
2025-03-04 15:14:41 +0530

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -83,8 +83,12 @@
             <div class="flex flex-col h-full">
               <div class="border-gray-200 rounded bg-gray-50 flex-auto flex justify-center items-center font-medium">
                 <div class="card flex flex-col items-center justify-center py-10 px-6">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="green" class="h-14 w-14 m-2">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+</svg>
+
                   <h2 class="text-xl font-bold">Thank You , {{ clientName }}!</h2>
-                  <p class="text-lg text-gray-700 mt-2">Your IP verification is completed.</p>
+                  <p class="text-lg text-gray-700 mt-2 flex justify-center text-center ">Your IP verification is completed.</p>
                   <div class="mt-4">
                     <Button label="Proceed to E-Sign" @click="proceedToESign"
                       class="bg-blue-600 text-white hover:bg-blue-700 transition duration-200" />
