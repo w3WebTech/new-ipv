163aaaf87c939f27ee920c6c8e55cca26556a391
w3WebTech <164163622+w3WebTech@users.noreply.github.com>
2025-03-05 13:18:13 +0530

    fix

--- pages/test.vue
+++ pages/test.vue
@@ -6,7 +6,7 @@
         <div class="px-5 my-3"> In person Verification</div>
       </div>
 
-      <StepPanels class="px-5 my-5">
+      <StepPanels class="px-5 my-3">
         <!-- Step 1 Panel -->
         <StepPanel v-slot="{ activateCallback }" value="1">
           <div class="border-2 border-dashed">
@@ -15,7 +15,7 @@
                 <div class="card px-5 py-10">
                   <div class="gap-2">
                     <!-- Check if captured image is available -->
-                    <div v-if="capturedImage" class="card flex flex-col items-center justify-center py-10 ">
+                    <div v-if="capturedImage" class="card flex flex-col items-center justify-center py-2 ">
                       
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="green" class="h-14 w-14 m-2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
